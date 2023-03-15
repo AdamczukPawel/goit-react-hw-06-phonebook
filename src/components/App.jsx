46de30481db6608e85212028561f6941ca8ca462
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2023-03-15 18:27:57 +0100

    Fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,8 @@ import css from './App.module.css';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { ContactForm } from './ContactForm/ContactForm';
-import { useEffect } from 'react';
-import { setContactsAction } from 'redux/contacts/contacts.slice';
 
 export const App = () => {
-  useEffect(() => {
-    const contacts = localStorage.getItem('contacts');
-    if (contacts) {
-      setContactsAction(JSON.parse(contacts));
-    }
-  }, []);
-
   return (
     <div>
       <h1 className={css.title}>Phonebook</h1>
